9b024b5f0edc63606df0736dcf4b4c434086f9c9
aoqlsdl <bini92444@gmail.com>
2023-03-15 22:53:21 +0900

    📝docs: Problem1 풀이

--- src/main/java/onboard/Problem1.java
+++ src/main/java/onboard/Problem1.java
@@ -6,6 +6,21 @@ import java.util.List;
 public class Problem1 {
     public static List<Integer> solution(Integer hour, Integer minute){
         List<Integer> answer = new ArrayList<>();
+
+        // 45분 앞당기기
+        minute -= 45;
+
+        // minute 값이 0보다 작으면 minute에 +60, hour -1
+        if(minute<0) {
+            hour--;
+            minute += 60;
+        }
+        // hour 값이 0보다 작으면 hour에 +24
+        if(hour<0) hour = 24+hour;
+
+        answer.add(hour);
+        answer.add(minute);
+
         return answer;
     }
 }
